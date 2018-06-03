48ba3c1818707e118a790dff9b8ce377215d4bf7
ZhongWanYuXi <youjiniot@163.com>
2018-06-03 21:54:21 +0800

    201806032200

--- hfzadmin/src/main/java/com/hfz/hfzadmin/HfzadminApplication.java
+++ hfzadmin/src/main/java/com/hfz/hfzadmin/HfzadminApplication.java
@@ -6,12 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @Configurable
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
 @EnableAutoConfiguration
+@EnableTurbine
+@EnableHystrixDashboard
+@EnableHystrix
 public class HfzadminApplication {
 
 	public static void main(String[] args) {
