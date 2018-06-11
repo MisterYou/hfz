5900bf067818e896c1d564adbd89e8b91a2bedda
MisterYou <1206919076@qq.com>
2018-06-11 20:03:53 +0800

    201806112003

---
+++ uaa-service/src/main/java/comhfz/uaaservice/UaaServiceApplication.java
@@ -0,0 +1,16 @@
+package comhfz.uaaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+public class UaaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaaServiceApplication.class, args);
+	}
+}
