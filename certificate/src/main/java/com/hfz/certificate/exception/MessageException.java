50b8cba4d8f6e69994a4cab07825726106760a1c
MisterYou <1206919076@qq.com>
2018-06-25 20:04:33 +0800

    201806252004

---
+++ certificate/src/main/java/com/hfz/certificate/exception/MessageException.java
@@ -0,0 +1,18 @@
+package com.hfz.certificate.exception;
+
+/**
+ * @author lwq
+ * @date 2018/4/24
+ * @comment 简单的消息提示异常
+ */
+public class MessageException extends RuntimeException {
+
+    public MessageException() {
+        super();
+    }
+
+    public MessageException(String message) {
+        super(message);
+    }
+
+}
